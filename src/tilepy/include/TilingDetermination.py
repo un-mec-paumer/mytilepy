308b35ba5d3ce20f5aa9bc9351634c8b84ae247a
aciric <antoine.ciric@cea.fr>
2026-08-27 15:58:52 +0200

    merge from original

--- src/tilepy/include/TilingDetermination.py
+++ src/tilepy/include/TilingDetermination.py
@@ -245,7 +245,7 @@ def PGWinFoV(skymap, nameEvent, obspar, dirName, task_id=None):
                     )
                     if P_GW <= obspar.minProbcut:
                         logger.info(
-                            f"Condition not met at {ObservationTime}: Pgw= {P_GW.quantity[0]:.4f} must be greater than ({obspar.minProbcut:.4f})"
+                            f"Condition not met at {ObservationTime}: Pgw= {float(P_GW):.4f} must be greater than ({obspar.minProbcut:.4f})"
                         )
                     else:
                         Round.append(2)
